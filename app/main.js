a29cfe068912fd15eee32d38a8eb1febb81d2e0d
fengjb <18794758986@163.com>
2017-07-20 20:40:49 +0800

    app文件

---
+++ app/main.js
@@ -0,0 +1,4 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var AppComponent = require('./components/productBox.js')
+ReactDOM.render(<AppComponent />, document.getElementById('content'))
